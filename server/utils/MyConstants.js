d86e02ab4e2defc8fff9e544350f8d1ac2c4a2b9
duychemgio <116256423+duychemgio@users.noreply.github.com>
2023-07-24 18:33:15 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,12 +1,11 @@
 const MyConstants = {
-  DB_SERVER: "cluster0.sxrsszp.mongodb.net",
-  DB_USER: "duongledc",
-  DB_PASS: "123456789a",
+  DB_SERVER: "cluster.r66w3v2.mongodb.net",
+  DB_USER: "nguyenhoanganhtuan987",
+  DB_PASS: "Anhtuan2002",
   DB_DATABASE: "shoppingonline",
-  EMAIL_USER: "Duongle.dc@gmail.com", // Microsoft mail service
-  EMAIL_PASS: "zwbssjpkiatojaqg",
+  EMAIL_USER: "nguyenhoanganhtuan987@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "bsxhgkefubeghqiy",
   JWT_SECRET: "a",
   JWT_EXPIRES: "10000000", // in milliseconds
 };
-
 module.exports = MyConstants;
