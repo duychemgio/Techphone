b9489aaa751fa034a240e6821eb329c333bfab62
duychemgio <phamminhduy8@gmail.com>
2023-07-24 15:29:42 +0700

    add 3 projects

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: "cluster1.sdhtbdy.mongodb.net",
+  DB_USER: "phamminhduy8",
+  DB_PASS: "a123456789",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "phamminhduy8@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "ibixhzpphyydexms",
+  JWT_SECRET: "a",
+  JWT_EXPIRES: "10000000", // in milliseconds
+};
+
+module.exports = MyConstants;
